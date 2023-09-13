e5b91b340222ca329bbfe9155b103e7f7f53324a
Mel Boyajian <MelBoyajian@MelBoyaiansiMac.attlocal.net>
2023-09-12 21:41:01 -0700

    blogly part 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -20,23 +20,27 @@ db.create_all()
 
 @app.route("/")
 def base():
-    """Redirect to list of users."""
-    return redirect("/users")
+    """Change the user page to show the posts for that user."""
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
 
+"""User route"""
+"""Show all users. Make these links to view the detail page for the user. Have a link here to the add-user form."""
 @app.route("/users")
 def users_index():
-    """Show all users. Make these links to view the detail page for the user. Have a link here to the add-user form."""
-    users = User.query.order_by(User.first_name, User.last_name).all()
+    users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template("users/index.html", users=users)
 
+"""Show an add form for users"""
 @app.route("/users/new", methods=["GET"])
-def user_new_form():
+def users_new_form():
     return render_template("users/new.html")
 
+"""Process the add form, adding a new user and going back to /users"""
 @app.route("/users/new", methods = ["POST"])
-def handle_form():
-    """Handles the form submission"""
-    new_user = User(
+def users_new():
+    
+    new_user = User (
         first_name=request.form['first_name'],
         last_name=request.form['last_name'],
         image_url=request.form['image_url'] or None)
@@ -44,18 +48,21 @@ def handle_form():
     db.session.commit()
     return redirect("/users")
 
+"""Show information about the given user. Have a button to get to their edit page, and to delete the user."""
 @app.route("/users/<int:user_id>")
-def show_users(user_id):
+def users_show(user_id):
     user = User.query.get_or_404(user_id)
     return render_template("users/show.html", user=user)
 
+"""Show the edit page for a user. Have a cancel button that returns to the detail page for a user, and a save button that updates the user."""
 @app.route("/users/<int:user_id>/edit")
-def edit_users(user_id):
+def users_edit(user_id):
     user = User.query.get_or_404(user_id)
     return render_template("users/edit.html", user=user)
 
+"""Process the edit form, returning the user to the /users page."""
 @app.route("/users/<int:user_id>/edit", methods=["POST"])
-def update_users(user_id):
+def users_update(user_id):
     user = User.query.get_or_404(user_id)
     user.first_name=request.form["first_name"]
     user.last_name=request.form["last_name"]
@@ -66,11 +73,62 @@ def update_users(user_id):
 
     return redirect("/users")
 
-
+"""Delete the user."""
 @app.route("/users/<int:user_id>/delete", methods=["POST"])
-def delete_user(user_id):
+def users_delete(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
 
     return redirect("/users")
+
+"""Post route"""
+"""Show form to add a post for that user."""
+@app.route("/users/<int:user_id>/posts/new")
+def posts_new_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("posts/new.html", user=user)
+
+"""Handle add form; add post and redirect to the user detail page."""
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def posts_new(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user=user)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/users/{user_id}")
+
+"""Show a post. Show buttons to edit and delete the post."""
+@app.route("/posts/<int:post_id>")
+def show_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("posts/show.html", post=post)
+
+"""Show form to edit a post, and to cancel (back to user page)."""
+@app.route("/posts/<int:post_id>/edit", methods=["GET"])
+def posts_edit(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("posts/edit.html", post=post)
+
+"""Handle editing of a post. Redirect back to the post view."""
+@app.route("/posts/<int:post_id>/edit", methods=["POST"])
+def posts_update(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form["title"]
+    post.content = request.form["content"]
+
+    db.session.add(post)
+    db.session.commit()
+    return redirect(f"/users/{post.user_id}")
+
+"""Delete the post."""
+@app.route("/posts/<int:post_id>/delete", methods=["POST"])
+def delete_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,11 +13,27 @@ class User(db.Model):
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
-
+    """Add foreign key to the User table"""
+    posts = db.relationship("Post", backref='user', cascade="all, delete-orphan")
     """The @property decorator is used to create a property method called full_name for the User class. the @property decorator is used to define a method as a "getter" method for a class attribute."""
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
     
 def connect_db(app):
     """Connect to database."""
